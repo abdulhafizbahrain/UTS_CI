b9c124d054ea0eaea131e308e60f6b4c7c071498
abdulhafizbahrain <abdulhafizbahrain@gmail.com>
2018-12-01 17:28:33 +0800

    membuat tombol menu print

--- application/views/pengguna/cetak.php
+++ application/views/pengguna/cetak.php
@@ -1,3 +1,5 @@
+<?php// Skrip berikut ini adalah skrip yang bertugas untuk meng-export data tadi ke excellheader("Content-type: application/vnd-ms-excel");header("Content-Disposition: attachment; filename=nama_filenya.xls");?>
+
 <!DOCTYPE html>
 <html>
 <head>
